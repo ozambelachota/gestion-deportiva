0482b1abe1ec523d890e90b44e59b59dad0e950d
ozambelachota <haroldozambelachota@outlook.com>
2024-02-05 16:53:12 -0500

    theme in admin

--- src/routes/protected.routes.tsx
+++ src/routes/protected.routes.tsx
@@ -1,24 +1,13 @@
-import { ThemeProvider, createTheme } from "@mui/material";
 import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useUserStore } from "../store/login.store";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
 
 function ProtectedRouter() {
   const rol = useUserStore((state) => state.rol);
   useEffect(() => {}, [rol]);
 
   if (rol === "admin") {
-    return (
-      <ThemeProvider theme={darkTheme}>
-        <Outlet />
-      </ThemeProvider>
-    );
+    return <Outlet />;
   }
   return <Navigate to="/" />;
 }
