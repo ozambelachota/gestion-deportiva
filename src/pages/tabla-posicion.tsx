5cb7c11676da377320fbf6a48a86899a7a921b42
ozambelachota <haroldozambelachota@outlook.com>
2024-04-13 16:38:24 -0500

    swiper and correction table posicion

--- src/pages/tabla-posicion.tsx
+++ src/pages/tabla-posicion.tsx
@@ -19,8 +19,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { PosicionStore } from "../store/PosicionStore";
 
 import { type TablaPosicion } from "../types/fixture.api.type";
@@ -70,11 +68,7 @@ const TablaPosicionPage: React.FC = () => {
   return (
     <>
       <PDFDownloadLink
-        document={
-          <PDFGenerator
-            groupsTabla={groupsTabla}
-          />
-        }
+        document={<PDFGenerator groupsTabla={groupsTabla} />}
         fileName="tbl_posicion"
       >
         {({ loading, error }) => {
@@ -129,7 +123,10 @@ const TablaPosicionPage: React.FC = () => {
                           return (
                             <TableRow key={equipo.id}>
                               <TableCell>
-                                {equipo.promocion_participante?.nombre_promocion}
+                                {
+                                  equipo.promocion_participante
+                                    ?.nombre_promocion
+                                }
                               </TableCell>
                               <TableCell>{equipo.pj}</TableCell>
                               <TableCell>{equipo.pg}</TableCell>
@@ -175,104 +172,6 @@ const TablaPosicionPage: React.FC = () => {
           </Box>
         )}
       </div>
-
-      <style>{`
-        @media (max-width: 768px) {
-          .tabla-container {
-            display: none;
-          }
-        }
-      `}</style>
-
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-        className="swiper-container"
-      >
-        {Object.keys(groupsTabla).map((grupoId, index) => (
-          <SwiperSlide key={grupoId}>
-            <div className="swiper-slide">
-              <Typography marginTop={"8px"} textAlign={"center"} variant="h5">
-                Tabla de Posiciones - Grupo {grupoId}
-              </Typography>
-              <div style={{ overflowX: "auto" }}>
-                <TableContainer sx={{ fontSize: '1.2rem' }} component={Paper} className="rounded">
-                  <Table
-                    size="small"
-                    sx={{
-                      background: colorPalette[index],
-                      backgroundImage: "url(/table.png)",
-                    }}
-                  >
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>#</TableCell>
-                        <TableCell align="left">PJ</TableCell>
-                        <TableCell>PG</TableCell>
-                        <TableCell>PE</TableCell>
-                        <TableCell>PP</TableCell>
-                        <TableCell>GF</TableCell>
-                        <TableCell>GC</TableCell>
-                        <TableCell>DG</TableCell>
-                        <TableCell align="left">Puntos</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {groupsTabla[grupoId].map((equipo) => {
-                        return (
-                          <TableRow key={equipo.id}>
-                            <TableCell>
-                              {equipo.promocion_participante?.nombre_promocion}
-                            </TableCell>
-                            <TableCell>{equipo.pj}</TableCell>
-                            <TableCell>{equipo.pg}</TableCell>
-                            <TableCell>{equipo.pe}</TableCell>
-                            <TableCell>{equipo.pp}</TableCell>
-                            <TableCell>{equipo.goles_f}</TableCell>
-                            <TableCell>{equipo.goles_e}</TableCell>
-                            <TableCell>{equipo.diferencia_goles}</TableCell>
-                            <TableCell
-                              sx={{
-                                background: "url('/estrella-n.png')",
-                                backgroundSize: "2.7rem",
-                                backgroundPosition: "left",
-                                backgroundRepeat: "no-repeat",
-                              }}
-                              align="left"
-                            >
-                              {equipo.puntos}
-                            </TableCell>
-                          </TableRow>
-                        );
-                      })}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      <style>{`
-        .swiper-container {
-          display: none;
-        }
-        @media (max-width: 768px) {
-          .swiper-container {
-            display: block;
-          }
-          .tabla-container {
-            display: none;
-          }
-        }
-      `}</style>
     </>
   );
 };
