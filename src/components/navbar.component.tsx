eca630055dddd8b2932931ac1636f8f32a3aeb5f
ozambelachota <haroldozambelachota@outlook.com>
2024-06-12 22:20:59 -0500

    fixe: navbar modify

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,3 +1,4 @@
+import  { useState } from 'react';
 import {
   AccountCircle,
   CalendarMonth as CalendarMonthIcon,
@@ -8,7 +9,10 @@ import {
 import AlignVerticalBottomIcon from "@mui/icons-material/AlignVerticalBottom";
 import {
   AppBar,
+  Box,
   Button,
+  CssBaseline,
+  Divider,
   Drawer,
   IconButton,
   List,
@@ -16,129 +20,154 @@ import {
   ListItemIcon,
   ListItemText,
   Toolbar,
-  useMediaQuery,
+  Typography,
+  useMediaQuery
 } from "@mui/material";
-import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
-const Navbar = () => {
-  const isMobile = useMediaQuery("(max-width:800px)");
-  const [drawerOpen, setDrawerOpen] = useState(false);
+interface Props {
+  window?: () => Window;
+}
 
-  const toggleDrawer = (open: boolean) => () => {
-    setDrawerOpen(open);
+const drawerWidth = 240;
+
+const menuItems = [
+  {
+    text: "Resultados fecha anterior",
+    to: "/resultado",
+    icon: <CalendarMonthIcon />,
+  },
+  {
+    text: "Tabla de posiciones",
+    to: "/posicion",
+    icon: <AlignVerticalBottomIcon />,
+  },
+  { text: "Futbol", to: "/", icon: <SportsSoccerIcon color="success" /> },
+  {
+    text: "Voley y Voley Mixto",
+    to: "/voley",
+    icon: <SportsVolleyballIcon />,
+  },
+  {
+    text: "Goleadores",
+    to: "/goles",
+    icon: <SportsSoccerIcon color="warning" />,
+  },
+  {
+    text: "Sanciones",
+    to: "/sancion",
+    icon: <SportsSoccerIcon color="error" />,
+  },
+  { text: "Iniciar sesión", to: "/login", icon: <AccountCircle /> },
+];
+
+export default function Navbar(props: Props) {
+  const { window } = props;
+  const isMobile = useMediaQuery("(max-width:800px)");
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
-  const menuItems = [
-    {
-      text: "Resultados fecha anterior",
-      to: "/resultado",
-      icon: <CalendarMonthIcon />,
-    },
-    {
-      text: "Tabla de posiciones",
-      to: "/posicion",
-      icon: <AlignVerticalBottomIcon />,
-    },
-    { text: "Futbol", to: "/", icon: <SportsSoccerIcon color="success" /> },
-    {
-      text: "Voley y Voley Mixto",
-      to: "/voley",
-      icon: <SportsVolleyballIcon />,
-    },
-    {
-      text: "Goleadores",
-      to: "/goles",
-      icon: <SportsSoccerIcon color="warning" />,
-    },
-    {
-      text: "Sanciones",
-      to: "/sancion",
-      icon: <SportsSoccerIcon color="error" />,
-    },
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        EXAFAM
+      </Typography>
+      <Divider />
+      <List>
+        {menuItems.map((item) => (
+          <ListItem
+            key={item.text}
+            component={RouterLink}
+            to={item.to}
+            onClick={handleDrawerToggle}
+          >
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
-    { text: "Iniciar sesión", to: "/login", icon: <AccountCircle /> },
-  ];
+  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <AppBar position="sticky" className="w-full" color="secondary">
-      <Toolbar
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center", // Centro los elementos horizontalmente
-          padding: "0",
-          flexDirection: isMobile ? "column" : "row", // Alineación vertical en dispositivos móviles,
-          backgroundColor: "indigo",
-          opacity: 0.7,
-          color: "#fff",
-          transition: "height 0.3s ease",
-          gap: "1rem",
-          fontSize: "20rem",
-          height: "0.2em",
-        }}
-      >
-        {isMobile && (
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={toggleDrawer(true)}
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        )}
-        <Drawer
-          anchor="left"
-          open={drawerOpen}
-          onClose={toggleDrawer(false)}
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="sticky" component="nav" color="secondary">
+        <Toolbar
           sx={{
-            width: isMobile ? "100%" : "auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "0",
+            flexDirection: isMobile ? "column" : "row",
+            backgroundColor: "indigo",
+            opacity: 0.7,
+            color: "#fff",
+            transition: "height 0.3s ease",
+            gap: "1rem",
+            height: "0.2em",
           }}
         >
-          <List>
-            {menuItems.map((item) => (
-              <ListItem
-                key={item.text}
-                component={RouterLink}
-                to={item.to}
-                onClick={toggleDrawer(false)}
-              >
-                {item.icon && <ListItemIcon>{item.icon}</ListItemIcon>}
-                <ListItemText primary={item.text} />
-              </ListItem>
-            ))}
-          </List>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+          {!isMobile && (
+            <>
+              {menuItems.map((item) => (
+                <Button
+                  key={item.text}
+                  color="inherit"
+                  component={RouterLink}
+                  to={item.to}
+                  sx={{
+                    padding: "5px 0",
+                    fontSize: "1rem",
+                    color: "#fff",
+                    border: "1px solid #fff",
+                    margin: "15px",
+                    ":hover": {
+                      backgroundColor: "#cccccc",
+                    },
+                  }}
+                >
+                  {item.icon}
+                  {item.text}
+                </Button>
+              ))}
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
         </Drawer>
-        {!isMobile && (
-          <>
-            {menuItems.map((item) => (
-              <Button
-                key={item.text}
-                color="inherit"
-                component={RouterLink}
-                to={item.to}
-                sx={{
-                  padding: "5px 0",
-                  fontSize: "1rem",
-                  color: "#fff",
-                  border: "1px solid #fff",
-                  margin: "15px",
-                  ":hover": {
-                    backgroundColor: "#cccccc",
-                  },
-                }}
-              >
-                {item.icon}
-                {item.text}
-              </Button>
-            ))}
-          </>
-        )}
-      </Toolbar>
-    </AppBar>
+      </nav>
+    </Box>
   );
-};
-
-export default Navbar;
+}
