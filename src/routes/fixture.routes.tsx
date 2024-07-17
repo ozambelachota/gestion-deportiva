c85e9706df41334bf9e7e5271f40e8f7999b93af
ozambelachota <haroldozambelachota@outlook.com>
2024-07-17 10:41:24 -0500

    fixe: component lazy loading

--- src/routes/fixture.routes.tsx
+++ src/routes/fixture.routes.tsx
@@ -1,107 +1,154 @@
+import { CircularProgress } from "@mui/material";
+import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import FormEditSaancionComponent from "../components/edit-form-sancion.component";
-import FixtureCreate from "../components/fixture/fixture-create";
-import ResultFixtureFormPage from "../components/fixture/result-fixture-form";
-import FormPromocionParticipante from "../components/form-promocion-participante";
-import EditjugadorComponent from "../components/nomina/edit-jugador.component";
-import TablaFixture from "../components/tabla-fixture";
-import GolPage from "../pages/Gol.page";
-import Admin from "../pages/admin";
-import { FixturePage } from "../pages/fixture";
-import { default as SancionPage } from "../pages/gol-sancion";
-import Home from "../pages/home";
-import Layout from "../pages/layout";
-import LayoutAdmin from "../pages/layout-admin";
-import Login from "../pages/login";
-import NominaPage from "../pages/nomina";
-import Promocion from "../pages/promocion";
-import { RegisterPromocion } from "../pages/register-promocion";
-import ResultPage from "../pages/resultado";
-import Sancion from "../pages/sancion";
-import TablaPosicionPage from "../pages/tabla-posicion";
-import GrupoPosicionComponents from "../pages/tabla-posicion/grupo-posicion.component";
-import PosicionEditPage from "../pages/tabla-posicion/posicion-edit-promocion";
-import TablaEditPosicionPage from "../pages/tabla-posicion/tabla-edit.page";
-import VoleyPage from "../pages/voley";
-import { TablaVoleyPage } from "../pages/voley-posicion/voley-tabla.page";
-import ProtectedRouter from "./protected.routes";
-import { EditedPartido } from "../pages/partido/edit-partido.page";
 
+// Convertir las importaciones a React.lazy
+const FormEditSaancionComponent = React.lazy(
+  () => import("../components/edit-form-sancion.component")
+);
+const FixtureCreate = React.lazy(
+  () => import("../components/fixture/fixture-create")
+);
+const ResultFixtureFormPage = React.lazy(
+  () => import("../components/fixture/result-fixture-form")
+);
+const FormPromocionParticipante = React.lazy(
+  () => import("../components/form-promocion-participante")
+);
+const EditjugadorComponent = React.lazy(
+  () => import("../components/nomina/edit-jugador.component")
+);
+const TablaFixture = React.lazy(() => import("../components/tabla-fixture"));
+const GolPage = React.lazy(() => import("../pages/Gol.page"));
+const Admin = React.lazy(() => import("../pages/admin"));
+const FixturePage = React.lazy(() =>
+  import("../pages/fixture").then((module) => ({ default: module.FixturePage }))
+);
+const SancionPage = React.lazy(() => import("../pages/gol-sancion"));
+const Home = React.lazy(() => import("../pages/home"));
+const Layout = React.lazy(() => import("../pages/layout"));
+const LayoutAdmin = React.lazy(() => import("../pages/layout-admin"));
+const Login = React.lazy(() => import("../pages/login"));
+const NominaPage = React.lazy(() => import("../pages/nomina"));
+const Promocion = React.lazy(() => import("../pages/promocion"));
+const RegisterPromocion = React.lazy(() =>
+  import("../pages/register-promocion").then((module) => ({
+    default: module.RegisterPromocion,
+  }))
+);
+const ResultPage = React.lazy(() => import("../pages/resultado"));
+const Sancion = React.lazy(() => import("../pages/sancion"));
+const TablaPosicionPage = React.lazy(() => import("../pages/tabla-posicion"));
+const GrupoPosicionComponents = React.lazy(
+  () => import("../pages/tabla-posicion/grupo-posicion.component")
+);
+const PosicionEditPage = React.lazy(
+  () => import("../pages/tabla-posicion/posicion-edit-promocion")
+);
+const TablaEditPosicionPage = React.lazy(
+  () => import("../pages/tabla-posicion/tabla-edit.page")
+);
+const VoleyPage = React.lazy(() => import("../pages/voley"));
+const TablaVoleyPage = React.lazy(() =>
+  import("../pages/voley-posicion/voley-tabla.page").then((module) => ({
+    default: module.TablaVoleyPage,
+  }))
+);
+const ProtectedRouter = React.lazy(() => import("./protected.routes"));
+const EditedPartido = React.lazy(() =>
+  import("../pages/partido/edit-partido.page").then((module) => ({
+    default: module.EditedPartido,
+  }))
+);
 const RoutePublic = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/resultado" element={<ResultPage />} />
-        <Route path="/posicion" element={<TablaPosicionPage />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/voley" element={<VoleyPage />} />
-        <Route path="/sancion" element={<SancionPage />} />
-        <Route path="/goles" element={<GolPage />} />
-        <Route path="*" element={<Navigate to={"/"} />} />
-      </Routes>
-    </Layout>
+    <Suspense
+      fallback={
+        <div className="h-dvh w-dvw flex justify-center items-center bg-fuchsia-950">
+          <CircularProgress
+            color="primary"
+            className="flex justify-center align-items-center h-screen"
+          />
+        </div>
+      }
+    >
+      <Layout>
+        <Routes>
+          <Route path="/resultado" element={<ResultPage />} />
+          <Route path="/posicion" element={<TablaPosicionPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/voley" element={<VoleyPage />} />
+          <Route path="/sancion" element={<SancionPage />} />
+          <Route path="/goles" element={<GolPage />} />
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+      </Layout>
+    </Suspense>
   );
 };
 
 const RouterPrivate = () => {
   return (
-    <LayoutAdmin>
-      <Routes>
-        <Route element={<ProtectedRouter />}>
-          <Route path="home" element={<Admin />} />
-          <Route path="registrar-fixture" element={<FixturePage />} />
-          <Route path="registrar-promociones" element={<Promocion />} />
-          <Route
-            path="registrar-promociones/create/:id"
-            element={<RegisterPromocion />}
-          />
-          <Route path="partido/:id" element={<EditedPartido />} />
-          <Route path="fixture/create" element={<FixtureCreate />} />
-          <Route
-            path="promocion/create"
-            element={<FormPromocionParticipante />}
-          />
-          <Route path="nomina/:id" element={<NominaPage />} />
-          <Route path="nomina/edit/:id" element={<EditjugadorComponent />} />
-          <Route
-            path="result-fixture/:id"
-            element={<ResultFixtureFormPage />}
-          />
-          <Route path="sancion/create" element={<Sancion />} />
-          <Route path="sancion" element={<Sancion />} />
-          <Route
-            path="sancion/edit/:id" 
-            element={<FormEditSaancionComponent />}
-          />
-          <Route
-            path="posicionar-promocion"
-            element={<TablaEditPosicionPage />}
-          />
-          <Route
-            path="ver-posicion/promocion/grupo/:id"
-            element={<GrupoPosicionComponents />}
-          />
-          <Route path="voley/:deporte" element={<TablaVoleyPage />} />
-          <Route
-            path="posicion/edit/grupo/:id"
-            element={<PosicionEditPage />}
-          />
-          <Route path="home" element={<TablaFixture />} />
-        </Route>
-      </Routes>
-    </LayoutAdmin>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <LayoutAdmin>
+        <Routes>
+          <Route element={<ProtectedRouter />}>
+            <Route path="home" element={<Admin />} />
+            <Route path="registrar-fixture" element={<FixturePage />} />
+            <Route path="registrar-promociones" element={<Promocion />} />
+            <Route
+              path="registrar-promociones/create/:id"
+              element={<RegisterPromocion />}
+            />
+            <Route path="partido/:id" element={<EditedPartido />} />
+            <Route path="fixture/create" element={<FixtureCreate />} />
+            <Route
+              path="promocion/create"
+              element={<FormPromocionParticipante />}
+            />
+            <Route path="nomina/:id" element={<NominaPage />} />
+            <Route path="nomina/edit/:id" element={<EditjugadorComponent />} />
+            <Route
+              path="result-fixture/:id"
+              element={<ResultFixtureFormPage />}
+            />
+            <Route path="sancion/create" element={<Sancion />} />
+            <Route path="sancion" element={<Sancion />} />
+            <Route
+              path="sancion/edit/:id"
+              element={<FormEditSaancionComponent />}
+            />
+            <Route
+              path="posicionar-promocion"
+              element={<TablaEditPosicionPage />}
+            />
+            <Route
+              path="ver-posicion/promocion/grupo/:id"
+              element={<GrupoPosicionComponents />}
+            />
+            <Route path="voley/:deporte" element={<TablaVoleyPage />} />
+            <Route
+              path="posicion/edit/grupo/:id"
+              element={<PosicionEditPage />}
+            />
+            <Route path="home" element={<TablaFixture />} />
+          </Route>
+        </Routes>
+      </LayoutAdmin>
+    </Suspense>
   );
 };
 
 const FixtureRoutes = () => {
   return (
-    <>
+    <Suspense fallback={<div>Cargando...</div>}>
       <Routes>
         <Route path="*" element={<RoutePublic />} />
         <Route path="admin/*" element={<RouterPrivate />} />
       </Routes>
-    </>
+    </Suspense>
   );
 };
+
 export default FixtureRoutes;
